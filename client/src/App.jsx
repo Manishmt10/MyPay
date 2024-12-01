e3033c9ac7b97b8b071f7c383d470254d63e1983
Manish <mt11497@gmail.com>
2024-12-01 12:46:29 +0530

    redirect to signup route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,25 @@ import SendMoney from "./pages/SendMoney";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 
+function RedirectToSignup() {
+   const navigate = useNavigate();
+   useEffect(() => {
+      navigate("/signup");
+   },[navigate])
+
+   return null;
+}
+
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/send" element={<SendMoney/>}/>
+        <Route path="/" element={<RedirectToSignup/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/send" element={<SendMoney/>} />
       </Routes>
     </BrowserRouter>
   )
