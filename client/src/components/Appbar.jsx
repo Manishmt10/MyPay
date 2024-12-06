d169b8141189aa1c32667cb525b13ce0b1335655
Manish <mt11497@gmail.com>
2024-12-07 00:22:09 +0530

    added logout feature

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom" ;
+
 export default function Appbar ({name}) {
-    return <div className="shadow h-18 flex justify-between">
+
+   const navigate = useNavigate();
+
+   return <div className="shadow h-18 flex justify-between">
         <div className="flex flex-col justify-center h-full mt-2 ml-4 text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 drop-shadow-md">
             MyPay 
         </div>
         <div className="flex">
+            <div className="py-2">
+               <button onClick={() => {
+               localStorage.removeItem("token");
+               navigate("/signup");
+            }} className="px-4 py-2 mr-2 font-medium font-sans text-white bg-gray-800 border rounded-xl">
+                  Logout
+               </button>
+            </div>
             <div className="flex flex-col justify-center h-full mr-4">
                 Hello
             </div>
